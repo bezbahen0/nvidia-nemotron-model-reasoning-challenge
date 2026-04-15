5939dfc26865455fef5dc9a8d00969cf403d62e7
bezbahen0 <sokolevskid@gmail.com>
2026-04-15 12:43:16 +0300

    just for first debug

--- src/sft_train.py
+++ src/sft_train.py
@@ -145,7 +145,7 @@ def main():
         eval_steps=args.eval_steps,
         save_strategy="steps",
         save_steps=args.eval_steps,
-        save_total_limit=6, 
+        save_total_limit=3, 
         load_best_model_at_end=True,
         
         optim="adamw_torch",
