4029b4cef19916259245967b0f5ae91485b9c71b
bezbahen0 <sokolevskid@gmail.com>
2026-04-19 13:52:38 +0300

    fix critical bug: not train all the parameters, train only transformers, forgot about mamba

--- src/sft_train.py
+++ src/sft_train.py
@@ -125,7 +125,8 @@ def main():
         r=args.lora_r,
         lora_alpha=args.lora_alpha,
         lora_dropout=args.lora_dropout,
-        target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+        # Используем регулярное выражение организаторов для захвата Mamba и MLP слоев
+        target_modules=r".*\.(in_proj|out_proj|up_proj|down_proj)$",
         bias="none",
         task_type="CAUSAL_LM",
     )
