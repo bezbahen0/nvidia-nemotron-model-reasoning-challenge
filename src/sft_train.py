f3aae0e6c73743eacec81209d5aacb7374f67560
bezbahen0 <sokolevskid@gmail.com>
2026-04-20 10:44:24 +0300

    add target_modules, change optim to paged_adamw_8bit, for memory optimization

--- src/sft_train.py
+++ src/sft_train.py
@@ -125,7 +125,10 @@ def main():
         r=args.lora_r,
         lora_alpha=args.lora_alpha,
         lora_dropout=args.lora_dropout,
-        target_modules=r".*\.(q_proj|k_proj|v_proj|o_proj|gate_proj|up_proj|down_proj|in_proj|out_proj)$",
+        # TODO:
+        # Maybe add lm_head, can be good for math or else
+        # but required more VRAM (hidden_size*vocab_size matrix)
+        target_modules=r".*\.(q_proj|k_proj|v_proj|o_proj|gate_proj|up_proj|down_proj|in_proj|out_proj|embed_tokens|lm_head)$", 
         bias="none",
         task_type="CAUSAL_LM",
     )
@@ -145,15 +148,16 @@ def main():
         save_strategy="steps",
         save_steps=args.eval_steps,
         save_total_limit=2, 
+        metric_for_best_model="eval_loss",
+        greater_is_better=False
         load_best_model_at_end=True,
         
-        optim="adamw_torch",
+        optim="paged_adamw_8bit",
         gradient_checkpointing=True,
         lr_scheduler_type="cosine",
         warmup_ratio=0.1,
         max_length=args.max_seq_len,
         completion_only_loss=True, 
-        #group_by_length=True,
 
         dataloader_num_workers=4,
         dataloader_prefetch_factor=2,
