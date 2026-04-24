b185cf8375e50de529c08755337ba8db02dc4a3d
bezbahen0 <sokolevskid@gmail.com>
2026-04-24 04:28:42 +0000

    add dvc.lock after run

--- upload_folder.py
+++ upload_folder.py
@@ -3,9 +3,9 @@ import kagglehub
 #kagglehub.login()
 
 
-LOCAL_MODEL_DIR = 'models/sft_v0.0.5/'
+LOCAL_MODEL_DIR = 'models/sft_v0.0.6/'
 
-MODEL_SLUG = 'nemotron_sft_v0.0.5' # Replace with model slug.
+MODEL_SLUG = 'nemotron_sft_v0.0.6' # Replace with model slug.
 
 # Learn more about naming model variations at
 # https://www.kaggle.com/docs/models#name-model.
