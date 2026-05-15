8fa407045c3f22fca7217e6396952121da16d6bc
bezbahen0 <sokolevskid@gmail.com>
2026-05-15 16:29:28 +0000

    update prams for b200

--- src/sft_train.py
+++ src/sft_train.py
@@ -129,7 +129,7 @@ def main():
         args.model_id,
         device_map={"": local_rank},
         torch_dtype=torch.bfloat16,
-        #trust_remote_code=True,
+        trust_remote_code=False,
         use_cache=False,
         attn_implementation="flash_attention_2"
     )
