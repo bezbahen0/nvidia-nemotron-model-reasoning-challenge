e72ca11118143c716b6a34961c067ed0337bc96a
bezbahen0 <sokolevskid@gmail.com>
2026-05-19 12:52:16 +0300

    run on 1 epoch

--- src/sft_train.py
+++ src/sft_train.py
@@ -423,7 +423,7 @@ def main():
         eval_steps=args.eval_steps,
         save_strategy="steps",
         save_steps=args.eval_steps,
-        save_total_limit=6,
+        save_total_limit=2,
         metric_for_best_model="eval_loss",
         greater_is_better=False,
         load_best_model_at_end=True,
